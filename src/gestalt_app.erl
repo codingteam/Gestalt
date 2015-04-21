ae7d120e6cc4aaab6a4353c72af13e0c23fde9b5
Alexander Batischev <eual.jp@gmail.com>
2015-04-22 00:19:21 +0300

    Remove gestalt_app:start/0
    We only needed it to start the application using `erl`; that was
    a temporary approach and will never be used again.

--- src/gestalt_app.erl
+++ src/gestalt_app.erl
@@ -3,14 +3,11 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/0, start/2, stop/1]).
+-export([start/2, stop/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
-
-start() ->
-    gestalt_sup:start_link().
 
 start(_StartType, _StartArgs) ->
     gestalt_sup:start_link().
